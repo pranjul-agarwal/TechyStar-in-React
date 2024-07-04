b381de66d39e8c446759b0ea298c1589be395f8c
pranjul agarwal <agarwalpranjul353@gmail.com>
2024-07-04 18:08:16 +0530

    website done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
